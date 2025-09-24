4b93caa7469aecbcc273131c4c27df2dca6c8cb8
raynboez <raynboez@gmail.com>
2025-09-24 20:55:24 +0100

    nullable column

--- mtg-app/database/migrations/2025_09_22_080110_add_columns_to_decks_table.php
+++ mtg-app/database/migrations/2025_09_22_080110_add_columns_to_decks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('decks', function (Blueprint $table) {
-            $table->string('colours');
+            $table->string('colours')->nullable();
         });
     }
 
